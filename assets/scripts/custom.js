ddc260d34f10678b039de46e6dc41cdbb3e7140f
Banys Paulius <banys.paulius@yahoo.com>
2020-04-29 09:25:35 +1000

    Add files via upload

---
+++ assets/scripts/custom.js
@@ -0,0 +1,49 @@
+
+"use strict";
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show1");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show1')) {
+          openDropdown.classList.remove('show1');
+        }
+      }
+    }
+  }
+// function myFunction(drop1) {
+//     document.getElementById("myDropdown1").classList.toggle("show");
+//   }
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn1')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content1");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
